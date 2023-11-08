dc02b37d6362a7c68073ff8ab736d1b2c4c4c28a
medoum <mohamedoum2430@gmail.com>
2023-11-08 22:47:41 +0100

    security constants

--- src/main/java/com/pokemonapi/review/security/JWTGenerator.java
+++ src/main/java/com/pokemonapi/review/security/JWTGenerator.java
@@ -1,4 +1,45 @@
 package com.pokemonapi.review.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.apache.tomcat.websocket.AuthenticationException;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
 public class JWTGenerator {
+
+    private String generate(Authentication authentication){
+        String username = authentication.getName();
+        Date currentDate = new Date();
+        Date expireDate = new Date(currentDate.getTime() +SecurityConstants.JWT_EXPIRATION);
+
+        String token = Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(expireDate)
+                .signWith(SignatureAlgorithm.HS512, SecurityConstants.JWT_SECRET)
+                .compact();
+        return token;
+    }
+    public String getUsernameFromJWT(String token){
+        Claims claims = Jwts.parser()
+                .setSigningKey(SecurityConstants.JWT_SECRET)
+                .parseClaimsJwt(token)
+                .getBody();
+        return claims.getSubject();
+    }
+    public boolean validateToken(String token){
+        try {
+            Jwts.parser().setSigningKey(SecurityConstants.JWT_SECRET).parseClaimsJws(token);
+            return true;
+        }catch (Exception ex){
+            throw new AuthenticationCredentialsNotFoundException("Jwt was expired or incorrect");
+
+        }
+    }
 }
--- src/main/java/com/pokemonapi/review/security/SecurityConfig.java
+++ src/main/java/com/pokemonapi/review/security/SecurityConfig.java
@@ -1,24 +1,18 @@
-package com.pokemonreview.api.security;
+package com.pokemonapi.review.security;
 
 import com.pokemonapi.review.security.CustomUserDetailsService;
 import com.pokemonapi.review.security.JwtAuthEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.SecurityFilterChain;;
 
 @Configuration
 @EnableWebSecurity
@@ -26,29 +20,31 @@ public class SecurityConfig {
 
     private JwtAuthEntryPoint authEntryPoint;
     private CustomUserDetailsService userDetailsService;
-    @Autowired
-    public SecurityConfig(CustomUserDetailsService userDetailsService, JwtAuthEntryPoint authEntryPoint) {
-        this.userDetailsService = userDetailsService;
-        this.authEntryPoint = authEntryPoint;
-    }
 
+    @Autowired
+    public SecurityConfig(JwtAuthEntryPoint authEntryPoint, CustomUserDetailsService userDetailsService){
+        this.authEntryPoint = authEntryPoint;
+        this.userDetailsService = userDetailsService;
+
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .exceptionHandling()
-                .authenticationEntryPoint(authEntryPoint)
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
-                .antMatchers("/api/auth/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
-        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-        return http.build();
+                .authorizeHttpRequests((requests) -> {
+                    requests
+                            .requestMatchers("/api/auth/**").permitAll();
+//                            .requestMatchers("*/register").authenticated()
+//                            .requestMatchers( "*/protectedEndpoint", "*/userLogin").authenticated();
+
+
+                });
+
+        http.cors(c->c.disable()).csrf(cs->cs.disable());
+
+
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        return (SecurityFilterChain)http.build();
     }
 
     @Bean
@@ -62,8 +58,8 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public  JWTAuthenticationFilter jwtAuthenticationFilter() {
-        return new JWTAuthenticationFilter();
-    }
+//    @Bean
+//    public  JWTAuthenticationFilter jwtAuthenticationFilter() {
+//        return new JWTAuthenticationFilter();
+//    }
 }
